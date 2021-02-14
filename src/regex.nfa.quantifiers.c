ef083e486f274ca02034948a8a0eae3298b4169e
Loic BUCKWELL <lfalkau@e1r8p14.42.fr>
2021-02-14 10:27:30 +0100

    generic function for quqqntifiers

--- src/regex.nfa.quantifiers.c
+++ src/regex.nfa.quantifiers.c
@@ -6,14 +6,14 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 08:37:26 by lfalkau           #+#    #+#             */
-/*   Updated: 2021/02/13 09:07:55 by lfalkau          ###   ########.fr       */
+/*   Updated: 2021/02/14 10:25:14 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <regex.nfa.h>
 #include <stdlib.h>
 
-static t_ns	*wildcard(t_ns *beg, t_ns *end, const char **ptr)
+t_ns		*nfa_build_quantifier(t_ns *beg, t_ns *end, const char **ptr)
 {
 	t_ns		*new_beg;
 	t_ns		*new_end;
@@ -22,47 +22,10 @@ static t_ns	*wildcard(t_ns *beg, t_ns *end, const char **ptr)
 	if (nfa_surruond(beg, end, &new_beg, &new_end) < 0)
 		return (NULL);
 	pattern_epsilon(&epsilon);
-	link_add(beg, epsilon, new_end);
-	link_add(end, epsilon, new_beg);
+	if (**ptr != '+')
+		link_add(beg, epsilon, new_end);
+	if (**ptr != '?')
+		link_add(end, epsilon, new_beg);
 	(*ptr)++;
 	return (new_end);
-}
-
-static t_ns	*plus(t_ns *beg, t_ns *end, const char **ptr)
-{
-	t_ns		*new_beg;
-	t_ns		*new_end;
-	t_pattern	epsilon;
-
-	if (nfa_surruond(beg, end, &new_beg, &new_end) < 0)
-		return (NULL);
-	pattern_epsilon(&epsilon);
-	link_add(end, epsilon, new_beg);
-	(*ptr)++;
-	return (new_end);
-}
-
-static t_ns	*imark(t_ns *beg, t_ns *end, const char **ptr)
-{
-	t_ns		*new_beg;
-	t_ns		*new_end;
-	t_pattern	epsilon;
-
-	if (nfa_surruond(beg, end, &new_beg, &new_end) < 0)
-		return (NULL);
-	pattern_epsilon(&epsilon);
-	link_add(beg, epsilon, new_end);
-	(*ptr)++;
-	return (new_end);
-}
-
-t_ns		*nfa_build_quantifier(t_ns *b, t_ns *e, const char **p)
-{
-	if (**p == '*')
-		return (wildcard(b, e, p));
-	if (**p == '+')
-		return (plus(b, e, p));
-	if (**p == '?')
-		return (imark(b, e, p));
-	return (e);
 }
